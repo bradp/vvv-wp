6db184fff59fe1e7f7337482df71cf697116e7a2
Brad Parbs <brad@bradparbs.com>
2021-08-05 19:30:55 -0500

    Update"

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -27,6 +27,7 @@ if [[ ! -f "index.php" ]]; then
   noroot wp core download --locale="en_US" --version="latest"
 
   noroot wp core config --dbname="${DB_NAME}" --dbprefix="wp_" --dbuser=wp --dbpass=wp
+
   noroot wp config set AUTOMATIC_UPDATER_DISABLED true --raw
   noroot wp config set DISABLE_WP_CRON true --raw
   noroot wp config set WP_DEBUG_DISPLAY false --raw
@@ -35,8 +36,6 @@ if [[ ! -f "index.php" ]]; then
   noroot wp config set WP_DISABLE_FATAL_ERROR_HANDLER true --raw
   noroot wp config set WP_ENVIRONMENT_TYPE "'development'" --raw
   noroot wp config set WP_SCRIPT_DEBUG  true --raw
-  noroot wp config set WP_CONTENT_DIR "dirname( __FILE__ ) . '/content'" --raw
-  noroot wp config set WP_CONTENT_URL "'https://' . \$_SERVER['HTTP_HOST'] . '/content'" --raw
 
   noroot wp core install --url="${VVV_SITE_NAME}.test" --title="${VVV_SITE_NAME}" --admin_name="admin" --admin_email="admin@example.com" --admin_password="password"
 
@@ -48,13 +47,16 @@ if [[ ! -f "index.php" ]]; then
   noroot wp plugin install user-switching --activate
   noroot wp plugin install wp-crontrol --activate
 
+  noroot wp config set WP_CONTENT_DIR "dirname( __FILE__ ) . '/content'" --raw
+  noroot wp config set WP_CONTENT_URL "'https://' . \$_SERVER['HTTP_HOST'] . '/content'" --raw
+
   cd ../
 
   noroot mv wp/wp-content content
 
   echo "<?php" > index.php
   echo "define( 'WP_USE_THEMES', true );" >> index.php
-  echo "require_once 'wp/wp-blog-header.php';" >> index.php
+  echo "require_once( 'wp/wp-blog-header.php' );" >> index.php
 
 fi
 
